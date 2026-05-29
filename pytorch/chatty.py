e3c8b6245876700ed6e6d57ffd8fecfaf011a0c4
Matt Perry <mpcomplete@gmail.com>
2026-05-29 11:09:57 -0400

    Chatty scaffold

---
+++ pytorch/chatty.py
@@ -0,0 +1,213 @@
+# Chapter 4
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,    # Vocabulary size
+    "context_length": 1024, # Context length
+    "emb_dim": 768,         # Embedding dimension
+    "n_heads": 12,          # Number of attention heads
+    "n_layers": 12,         # Number of layers
+    "drop_rate": 0.1,       # Dropout rate
+    "qkv_bias": False       # Query-Key-Value bias
+}
+cfg = GPT_CONFIG_124M
+
+import torch
+import torch.nn as nn
+
+if torch.backends.mps.is_available():
+    device = torch.device("mps")   # Apple Silicon GPU (Metal)
+elif torch.cuda.is_available():
+    device = torch.device("cuda")  # NVIDIA GPU
+else:
+    device = torch.device("cpu")   # CPU fallback
+
+# 3.6.2
+# Parallelize MultiHeadAttentionWrapper by stitching the multiple heads into one giant matrix.
+class MultiHeadAttention(nn.Module):
+    def __init__(self, d_in, d_out, context_length, dropout, num_heads, qkv_bias=False):
+        super().__init__()
+        assert(d_out % num_heads == 0), "d_out must be divisible by num_heads"
+
+        self.d_out = d_out
+        self.num_heads = num_heads
+        self.head_dim = d_out // num_heads
+        self.W_query = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_key   = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.W_value = nn.Linear(d_in, d_out, bias=qkv_bias)
+        self.out_proj = nn.Linear(d_out, d_out)
+        self.dropout = nn.Dropout(dropout)
+        self.register_buffer(
+            'mask',
+            torch.triu(torch.ones(context_length, context_length), diagonal=1)
+        )
+
+    def forward(self, x):
+        b, num_tokens, d_in = x.shape
+        keys = self.W_key(x)
+        queries = self.W_query(x)
+        values = self.W_value(x)
+        # keys.shape = (b, num_tokens, d_out)
+
+        # Split each n*d_out matrix into num_heads copies.
+        keys = keys.view(b, num_tokens, self.num_heads, self.head_dim)
+        queries = queries.view(b, num_tokens, self.num_heads, self.head_dim)
+        values = values.view(b, num_tokens, self.num_heads, self.head_dim)
+
+        # Reshape into b, num_heads, num_tokens, head_dim.
+        keys = keys.transpose(1, 2)
+        queries = queries.transpose(1, 2)
+        values = values.transpose(1, 2)
+
+        # query_i dot key_j for each i,j in head.
+        attn_scores = queries @ keys.transpose(2, 3)
+        attn_scores.masked_fill_(self.mask.bool()[:num_tokens, :num_tokens], -torch.inf)
+        attn_weights = torch.softmax(attn_scores / keys.shape[-1]**0.5, dim=-1)
+        attn_weights = self.dropout(attn_weights)
+        context_vec = attn_weights @ values
+
+        # Reshape back into b, num_tokens, num_heads, head_dim.
+        context_vec = context_vec.transpose(1, 2)
+        # Then combine heads to b, num_tokens, d_out.
+        context_vec = context_vec.contiguous().view(b, num_tokens, self.d_out)
+        # And do this weird projection step.
+        context_vec = self.out_proj(context_vec)
+        return context_vec
+
+# 4.2 Layer normalization.
+# Converts input values so they're centered at 0 and have a variance of 1.
+# Important for numerical stability.
+class LayerNorm(nn.Module):
+    def __init__(self, emb_dim, eps=1e-5):
+        super().__init__()
+        self.eps = 1e-5
+        self.scale = nn.Parameter(torch.ones(emb_dim))
+        self.shift = nn.Parameter(torch.zeros(emb_dim))
+
+    def forward(self, x):
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True, unbiased=False)
+        norm_x = (x - mean) / torch.sqrt(var + self.eps)
+        return self.scale * norm_x + self.shift
+
+# GELU activation function instead of RELU for smoother gradient.
+class GELU(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return 0.5 * x * (1 + torch.tanh(
+            torch.sqrt(torch.tensor(2.0 / torch.pi)) *
+            (x + 0.044715 * torch.pow(x, 3))
+        ))
+
+# 4.4
+class FeedForward(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.layers = nn.Sequential(
+            nn.Linear(cfg["emb_dim"], 4*cfg["emb_dim"]),
+            GELU(),
+            nn.Linear(4*cfg["emb_dim"], cfg["emb_dim"]))
+    def forward(self, x):
+        return self.layers(x)
+
+# 4.6 Transformer.
+# A dozen copies of this will be in the final network.
+# Attention -> feed-forward with normalization, dropout, and GELU activation.
+class TransformerBlock(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.att = MultiHeadAttention(
+            d_in=cfg["emb_dim"],
+            d_out=cfg["emb_dim"],
+            context_length=cfg["context_length"],
+            num_heads=cfg["n_heads"],
+            dropout=cfg["drop_rate"],
+            qkv_bias=cfg["qkv_bias"])
+        self.ff = FeedForward(cfg)
+        self.norm1 = LayerNorm(cfg["emb_dim"])
+        self.norm2 = LayerNorm(cfg["emb_dim"])
+        self.drop_shortcut = nn.Dropout(cfg["drop_rate"])
+
+    def forward(self, x):
+        # Attention block
+        # "Shortcut connection" helps avoid gradients vanishing to 0 as we backprop to the start of network.
+        shortcut = x
+        # Normalizing before is called pre-layernorm and is better than post-layernorm... somehow.
+        x = self.norm1(x)
+        x = self.att(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+
+        # Feed forward block
+        shortcut = x
+        x = self.norm2(x)
+        x = self.ff(x)
+        x = self.drop_shortcut(x)
+        x = x + shortcut
+        return x
+
+# 4.7 Real GPT model.
+# Uses our real transformer block and layer normalization.
+class GPTModel(nn.Module):
+    def __init__(self, cfg):
+        super().__init__()
+        self.tok_emb = nn.Embedding(cfg["vocab_size"], cfg["emb_dim"])
+        self.pos_emb = nn.Embedding(cfg["context_length"], cfg["emb_dim"])
+        self.drop_emb = nn.Dropout(cfg["drop_rate"])
+
+        # Use a placeholder for TransformerBlock
+        self.trf_blocks = nn.Sequential(
+            *[TransformerBlock(cfg) for _ in range(cfg["n_layers"])])
+
+        # Use a placeholder for LayerNorm
+        self.final_norm = LayerNorm(cfg["emb_dim"])
+        self.out_head = nn.Linear(cfg["emb_dim"], cfg["vocab_size"], bias=False)
+
+    def forward(self, in_idx):
+        batch_size, seq_len = in_idx.shape
+        tok_embeds = self.tok_emb(in_idx)
+        pos_embeds = self.pos_emb(torch.arange(seq_len, device=in_idx.device))
+        x = tok_embeds + pos_embeds
+        x = self.drop_emb(x)
+        x = self.trf_blocks(x)
+        x = self.final_norm(x)
+        logits = self.out_head(x)
+        return logits
+
+# 4.8 Iteratively generating text using our model.
+# Generates `max_new_tokens`, each time taking the most recent `context_size` input tokens,
+# finding the index of the most probable output token, and appending it to the input for the
+# next iteration.
+def generate_text_simple(model, idx, max_new_tokens, context_size):
+    for _ in range(max_new_tokens):
+        idx_cond = idx[:, -context_size:]
+        with torch.no_grad():
+            logits = model(idx_cond)
+        logits = logits[:, -1, :]
+        probas = torch.softmax(logits, dim=-1)
+        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
+        idx = torch.cat((idx, idx_next), dim=1)
+    return idx
+
+import tiktoken
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+batch = []
+
+# 4.8 Generating text
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+start_context = "The pig is really"
+encoded = tokenizer.encode(start_context)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+
+model.eval()  # skips dropout,etc that are only used during training
+out = generate_text_simple(model, encoded_tensor, max_new_tokens=6, context_size=GPT_CONFIG_124M["context_length"])
+print("Output:", out)
+print("Output len=", len(out[0]))
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+print("Input:", start_context)
+print("Output:", decoded_text)
