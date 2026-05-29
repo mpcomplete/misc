9534e25c37a54405e2685744bd66fa1951d1365e
Matt Perry <mpcomplete@gmail.com>
2026-05-29 11:22:18 -0400

    rm unused

--- pytorch/chatty.py
+++ pytorch/chatty.py
@@ -175,21 +175,6 @@ class GPTModel(nn.Module):
         logits = self.out_head(x)
         return logits
 
-# 4.8 Iteratively generating text using our model.
-# Generates `max_new_tokens`, each time taking the most recent `context_size` input tokens,
-# finding the index of the most probable output token, and appending it to the input for the
-# next iteration.
-def generate_text_simple(model, idx, max_new_tokens, context_size):
-    for _ in range(max_new_tokens):
-        idx_cond = idx[:, -context_size:]
-        with torch.no_grad():
-            logits = model(idx_cond)
-        logits = logits[:, -1, :]
-        probas = torch.softmax(logits, dim=-1)
-        idx_next = torch.argmax(probas, dim=-1, keepdim=True)
-        idx = torch.cat((idx, idx_next), dim=1)
-    return idx
-
 import tiktoken
 
 tokenizer = tiktoken.get_encoding("gpt2")
