20500e407724348793fd9b2b30f3a6dcc1cfcd2a
Matt Perry <mpcomplete@gmail.com>
2026-06-15 13:53:56 -0400

    training

--- pytorch/chapter5.py
+++ pytorch/chapter5.py
@@ -64,11 +64,65 @@ def calc_loss_loader(data_loader, model, device, num_batches=None):
         total_loss += loss.item()
     return total_loss / num_batches
 
+# torch.manual_seed(123)
+# model = GPTModel(GPT_CONFIG_124M)
+# model.to(device)
+# with torch.no_grad():
+#     train_loss = calc_loss_loader(train_loader, model, device)
+#     val_loss = calc_loss_loader(train_loader, model, device)
+# print("Training loss:", train_loss)
+# print("Validation loss:", val_loss)
+
+# 5.3 pre-training
+def train_model_simple(model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    tokens_seen, global_step = 0, -1
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(input_batch, target_batch, model, device)
+            loss.backward()
+            optimizer.step()
+            tokens_seen += inupt_batch.numel()
+            global_step += 1
+
+            # Optional: just tracks our progress.
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(model, train_loader, val_loader, device, eval_iter)
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(tokens_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): "
+                      f"Train loss {train_loss:.3f}"
+                      f"Val loss {val_loss:.3f}")
+        # Optional: test our final results.
+        generate_and_print_sample(model, tokenizer, device, start_context)
+    return train_losses, val_losses, track_tokens_seen
+
+# Debugging, show our loss as we progress.
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()  # Temporarily disable training mode.
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()  # Temporarily disable training mode.
+    context_size = model.pos_emb.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(model=model, idx=encoded, max_new_tokens=50, context_size=context_size)
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replcae("\n", " "))
+    model.train()
+
 torch.manual_seed(123)
 model = GPTModel(GPT_CONFIG_124M)
 model.to(device)
-with torch.no_grad():
-    train_loss = calc_loss_loader(train_loader, model, device)
-    val_loss = calc_loss_loader(train_loader, model, device)
-print("Training loss:", train_loss)
-print("Validation loss:", val_loss)
+optimizer = torch.optim.AdamW(model.parameters(), lr=0.0004, weight_decay=0.1)
+train_losses, val_losses, track_tokens_seen = train_model_simple(
+    model, train_loader, val_loader, optimizer, device,
+    num_epochs=10, eval_freq=5, eval_iter=5, start_context="Every effort moves you", tokenizer=tokenizer)
